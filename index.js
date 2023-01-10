2ef742e9fd677f34cf360083d53b37b2c7baca49
priyanka <priyankaraju92@gmail.com>
2023-01-10 16:33:15 -0500

    mistakein event listener

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //console.log("hi");
 let playerScore = 0;
 let computerScore = 0;
+let count =0;
+
 // array for output options
 const arr = ["rock","paper","scissors"];
 // Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
 // We’ll use this function in the game to make the computer’s play. 
-function getComputerChoice(){
+function getComputerChoice() {
     const randomNumber = arr[Math.floor(Math.random()*arr.length)];
     // use the console to make sure this is returning the expected output before moving to the next step!
     // console.log(randomNumber);
@@ -16,56 +18,112 @@ function getComputerChoice(){
 // Write a function that plays a single round of Rock Paper Scissors.
 // The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-function playRound(playerSelection, computerSelection) {
-    
-    if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper') ||
-    (playerSelection === 'paper' && computerSelection === 'rock')){
-        // return statement
-        //  `You Win! ${playerSelection} beats ${computerSelection}` = value to return 
-        //  template literals
-        playerScore++;
-        return `You Win! ${playerSelection} beats ${computerSelection}>>> `;
-    }
-    if (playerSelection === computerSelection) {
-        return "It's a tie";
-    }
-    else {
-        computerScore++;
-        return `You Lose! ${computerSelection} beats ${playerSelection}>>> `;
-    }
-    
-    
-  }
-  
-    document.addEventListener('DOMContentLoaded', function() {
-        document.querySelectorAll('button').forEach(function(button) {
-            button.onclick = function() {
-                const computerSelection = getComputerChoice();
-                const playerSelection = button.dataset.value;
-                //using inner html to set the contents of your empty <div> to some text
-                // createElement method is used to make a new HTML element such as a <div>, button, paragraph
-                //let oneItem = document.createElement("div");
-    
-                let newItem = document.createElement("div");
-                // access value
-                //oneItem.innerHTML = `playerScore : ${playerScore}`;
-                
-                newItem.innerHTML = playRound(playerSelection, computerSelection) + `   player score = ${playerScore}` + `   computer score = ${computerScore}`;
+
+  document.addEventListener('DOMContentLoaded', function() {
+   
+   
+    document.querySelectorAll('button').forEach(function(button) {
         
-                // document.location.appendChild(newItem);
-                //document.getElementById("result").appendChild(oneItem);
+        button.onclick = function() {
+            
+            let playerSelection = button.dataset.value;
+            
+            for(let i=0;i<5; i++){
+
                 
-                document.getElementById("result").appendChild(newItem);
-                //console.log(playRound(playerSelection, computerSelection));
+            let computerSelection = getComputerChoice();
+            console.log('player',playerSelection);
+            console.log('computer',computerSelection);
+            count++;
+            console.log('count',count);
+            
+        //using inner html to set the contents of your empty <div> to some text
+        // createElement method is used to make a new HTML element such as a <div>, button, paragraph
+              
+            
+            
+            
+        // access value
+            let result = playRound(playerSelection, computerSelection);
+            
+            let newItem = document.createElement("div");
+            newItem.innerHTML = result;
+        // document.location.appendChild(newItem);
+                
+            document.getElementById("result").appendChild(newItem);
             
             }
+            if(playerScore>computerScore){
             
+                let oItem = document.createElement("div");
+                oItem.innerHTML = `you are the winner`;
+                document.getElementById("result").appendChild(oItem);
+            }
+                else {
+                    let oItem = document.createElement("div");
+                    oItem.innerHTML = `computer computer! is the winner`;
+                    document.getElementById("result").appendChild(oItem);
+                }
+        }
             
-            
+        
+                //console.log(playRound(playerSelection, computerSelection));
         });
-
+    
+    
     });
+    
+    
+    function playRound(playerSelection, computerSelection) {
+       
+        //let computerSelection = getComputerChoice();
+        if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')){
+            // return statement
+            //  `You Win! ${playerSelection} beats ${computerSelection}` = value to return 
+            //  template literals
+          
+            playerScore = playerScore+1;
+            console.log('playerScore',playerScore);
+            let player = `You Win! ${playerSelection} beats ${computerSelection}>>> `;
+            return player;
+        }
+        if (playerSelection === computerSelection) {
+           
+            return "It's a tie";
+        }
+        else {
+            
+            computerScore++;
+            console.log('computerScore',computerScore);
+            let computer = `You Lose! ${computerSelection} beats ${playerSelection}>>> `;
+            return computer;
+        }
+        
+    }
+
+    
+    
+    
+    
+   /* if(playerScore>computerScore){
+            
+        let oItem = document.createElement("div");
+        oItem.innerHTML = `you are the winner`;
+        document.getElementById("result").appendChild(oItem);
+    }
+        else {
+            let oItem = document.createElement("div");
+            oItem.innerHTML = `computer computer! is the winner`;
+            document.getElementById("result").appendChild(oItem);
+        }
+        */
+
+        
+
+      
+
     
 //const playerSelection = "rock"; 
 //const computerSelection = getComputerChoice();
