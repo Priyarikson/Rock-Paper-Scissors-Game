1339b32e7870e0280d39348c94e8f872a027ed66
priyanka <priyankaraju92@gmail.com>
2023-01-11 11:12:53 -0500

    much better now

--- index.js
+++ index.js
@@ -1,74 +1,168 @@
+//console.log("hi");
+let playerScore = 0;
+let computerScore = 0;
+let count =0;
 
+// array for output options
 const arr = ["rock","paper","scissors"];
-
+// Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
+// We’ll use this function in the game to make the computer’s play. 
 function getComputerChoice() {
     const randomNumber = arr[Math.floor(Math.random()*arr.length)];
+    // use the console to make sure this is returning the expected output before moving to the next step!
+    // console.log(randomNumber);
     
     return randomNumber;
     
 }
 
-function checkWinner(playerSelection, computerSelection) {
+
+function playRound(playerSelection, computerSelection) {
+       
+    //let computerSelection = getComputerChoice();
     if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'scissors' && computerSelection === 'paper') ||
     (playerSelection === 'paper' && computerSelection === 'rock')){
-        return 'player';
-    }
-    if (playerSelection === computerSelection) {
-       
-        return "tie";
-    }
-    else {
-      
-        return 'computer';
-    }
-}
-
-function playRound(playerSelection,computerSelection){
-    let result = checkWinner(playerSelection, computerSelection);
-    if(result == 'player'){
+        // return statement
+        //  `You Win! ${playerSelection} beats ${computerSelection}` = value to return 
+        //  template literals
+        count++;
+        playerScore = playerScore+1;
+        console.log('playerScore',playerScore);
         let player = `You Win! ${playerSelection} beats ${computerSelection}>>> `;
         return player;
     }
-    else if(result == 'tie'){
+    if (playerSelection === computerSelection) {
+        count++;
         return "It's a tie";
     }
-    else{
+    else {
+        count++;
+        computerScore++;
+        console.log('computerScore',computerScore);
         let computer = `You Lose! ${computerSelection} beats ${playerSelection}>>> `;
         return computer;
     }
-}
-
-function game(){
-    let playerScore=0;
-    let computerScore=0;
-    for(let i=0; i<5; i++){
-        let playerSelection=getPlayerChoice();
-        let computerSelection=getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
-        if((checkWinner(playerSelection, computerSelection))=="player"){
-            playerScore++;
-        }
-        else if((checkWinner(playerSelection, computerSelection))=="computer")
-            {
-            computerScore++;
-        }
-    }
-        if(playerScore>computerScore){
-            console.log('player winsssss');
-        }
-        else if(playerScore<computerScore){
-            console.log('computer winsssss');
-        }
-        else{
-            console.log('tie');
-        }
     
 }
-game()
 
-function getPlayerChoice(){
-    let choice = prompt("rock,paper,scissors").toLowerCase();
-    return choice;
-}
+// calling the function to test
+// getComputerChoice();
+// Write a function that plays a single round of Rock Paper Scissors.
+// The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 
+  document.addEventListener('DOMContentLoaded', function() {
+   
+   
+    document.querySelectorAll('button').forEach(function(button) {
+        
+        button.onclick = function() {
+            
+            let playerSelection = button.dataset.value;
+            let computerSelection = getComputerChoice();
+            console.log('player',playerSelection);
+            console.log('computer',computerSelection);
+           
+            
+        //using inner html to set the contents of your empty <div> to some text
+        // createElement method is used to make a new HTML element such as a <div>, button, paragraph
+              
+            
+            
+            
+        // access value
+            let result = playRound(playerSelection, computerSelection);
+            
+            let newItem = document.createElement("div");
+            newItem.innerHTML = result;
+        // document.location.appendChild(newItem);
+                
+            document.getElementById("result").appendChild(newItem);
+            
+            if(count % 5 ===0 ){
+                
+            if(playerScore>computerScore){
+            
+                let oItem = document.createElement("div");
+                oItem.innerHTML = `you are the winner`;
+                document.getElementById("result").appendChild(oItem);
+                playerScore = 0;
+                computerScore = 0;
+            }
+                else {
+                    let oItem = document.createElement("div");
+                    oItem.innerHTML = `computer computer! is the winner`;
+                    document.getElementById("result").appendChild(oItem);
+                    playerScore = 0;
+                    computerScore = 0;
+                }
+        }
+    }
+            
+        
+                //console.log(playRound(playerSelection, computerSelection));
+        });
+    
+    
+    });
+    
+    
+    
+    
+    
+    
+    
+   /* if(playerScore>computerScore){
+            
+        let oItem = document.createElement("div");
+        oItem.innerHTML = `you are the winner`;
+        document.getElementById("result").appendChild(oItem);
+    }
+        else {
+            let oItem = document.createElement("div");
+            oItem.innerHTML = `computer computer! is the winner`;
+            document.getElementById("result").appendChild(oItem);
+        }
+        */
+
+        
+
+      
+
+    
+//const playerSelection = "rock"; 
+//const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
+
+//function getPlayerChoice() {
+    //while(true){
+        // case-insensitive (so users can input rock, ROCK, RocK or any other variation), .toLowerCase()
+        
+        //const playerChoice = prompt("Rock, Paper or Scissors").toLowerCase();
+        
+        //if(arr.includes(playerChoice)){
+            //return playerChoice;
+        //}
+        //continue;
+    //}
+        
+//}
+    
+
+//function game() {
+    //Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+    //for (let i = 0; i < 5; i++) {
+        // your code here!
+        //const playerSelection = getPlayerChoice();
+        //const computerSelection = getComputerChoice();
+        //console.log(playRound(playerSelection, computerSelection));
+    //}
+    //if(playerScore > computerScore){
+        //console.log("you are the winner");
+    //}
+    //else{
+        //console.log("computer computer! is the winner ");
+    //}
+//}   
+//game();
